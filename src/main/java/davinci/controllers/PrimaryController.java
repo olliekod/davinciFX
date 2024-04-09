6a78df4ff0b26fb900e120ff06c28555ecf54710
Jackson Dawson <jwd2464@gmail.com>
2024-04-09 18:20:12 -0400

    Added fmxl files and fixed issue

--- src/main/java/davinci/controllers/PrimaryController.java
+++ src/main/java/davinci/controllers/PrimaryController.java
@@ -10,4 +10,5 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
 }
--- src/main/java/davinci/library/App.java
+++ src/main/java/davinci/library/App.java
@@ -1,6 +1,7 @@
 package davinci.library;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
