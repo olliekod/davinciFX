914b4580426438b87c6d8fb30a9152e22e90f369
Jackson Dawson <jwd2464@gmail.com>
2024-04-09 13:48:44 -0400

    Added JSON Simple fix

--- src/main/java/davinci/model/CourseList.java
+++ src/main/java/davinci/model/CourseList.java
@@ -1,3 +1,4 @@
+package davinci.model;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module davinci {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens davinci to javafx.fxml;
     exports davinci;
