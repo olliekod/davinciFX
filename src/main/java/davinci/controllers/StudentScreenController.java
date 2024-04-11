d999f6a1889b712ef162d3512357026ce4f208c7
Shreklord <goldhama@email.sc.edu>
2024-04-10 20:10:19 -0400

    working on 8semesterplan

--- src/main/java/davinci/controllers/StudentScreenController.java
+++ src/main/java/davinci/controllers/StudentScreenController.java
@@ -4,15 +4,22 @@ import davinci.library.App;
 import davinci.model.User;
 import davinci.model.Student;
 import davinci.model.Major;
+import davinci.model.Course;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Observable;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 
 public class StudentScreenController implements Initializable {
 
+    User u = App.user;
+    Student s = App.student;
     @FXML
     private Label nameLabel;
 
@@ -25,20 +32,39 @@ public class StudentScreenController implements Initializable {
     @FXML
     private Label MajorLabel;
 
+    @FXML
+    private Label StandingLabel;
+
+    @FXML
+    private ListView eightSemesterPlanListView;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("init");
-        User u = App.user;
-        Student s = App.student;
+        
         String usersName = "Name: " + u.getFirstName() + " " + u.getLastName();
         String studentID = "ID: " + u.getStudentID();
         String GPA = "GPA: " + s.getGPAString();
         String major = "Major: " + s.getMajor().getName();
+        String standing = "Standing: " + s.getStanding();
+        
+
+        //displayEightSemesterPlan();
         nameLabel.setText(usersName);
         StudentIDLabel.setText(studentID);
         GPALabel.setText(GPA);
         MajorLabel.setText(major);
+        StandingLabel.setText(standing);
 
+    }
+
+    private void displayEightSemesterPlan(){
+        ObservableList<String> eightSemesterPlan = FXCollections.observableArrayList();
+
+        for(Course course : s.displayEightSemesterPlan()) {
+            eightSemesterPlan.add(course.getTitle());
+        }
+        eightSemesterPlanListView.setItems(eightSemesterPlan);
     }
 
 }
