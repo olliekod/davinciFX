43122cfa98102c8a9608b19020fa715861ca783d
Oliver Meihls <omeihls@email.sc.edu>
2024-04-14 19:01:08 -0400

    Added eight semester plan screen. Adjusted some other screens

---
+++ src/main/java/davinci/controllers/StudentNotesController.java
@@ -0,0 +1,25 @@
+package davinci.controllers;
+
+import davinci.library.App;
+import davinci.model.User;
+import davinci.model.Student;
+import davinci.model.Major;
+import davinci.model.Course;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Observable;
+import java.util.ResourceBundle;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+
+public class StudentNotesController {
+    
+}
