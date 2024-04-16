644472542a615f0c99ad01af08c9f1ba33a60ff2
Jackson Dawson <jwd2464@gmail.com>
2024-04-16 15:43:10 -0400

    Populated List View of Courses with Proper Information, Major isnt properly done though. Semester three is null and Six only has two classes

--- src/main/java/davinci/controllers/EightSemController.java
+++ src/main/java/davinci/controllers/EightSemController.java
@@ -26,38 +26,142 @@ public class EightSemController implements Initializable{
     Student s = App.student;
 
     @FXML
-    private TableView semesterOnePlanTable;
+    private ListView semesterOneListView;
 
     @FXML 
-    private TableView semesterTwoPlanTable;
+    private ListView semesterTwoListView;
 
     @FXML
-    private TableView semesterThreePlanTable;
+    private ListView semesterThreeListView;
 
     @FXML
-    private TableView semesterFourPlanTable;
+    private ListView semesterFourListView;
 
     @FXML
-    private TableView semesterFivePlanTable;
+    private ListView semesterFiveListView;
 
     @FXML
-    private TableView semesterSixPlanTable;
+    private ListView semesterSixListView;
 
     @FXML
-    private TableView semesterSevenPlanTable;
+    private ListView semesterSevenListView;
 
     @FXML
-    private TableView semesterEightPlanTable;
+    private ListView semesterEightListView;
 
-    @FXML
-    private ObservableList<Course> courses;
 
     @Override
     public void initialize(URL url, ResourceBundle rb)  {
-        courses = FXCollections.observableArrayList(); // Create an ObservableList
-        for(Course c : s.displayEightSemesterPlan()) {
-            courses.add(c);
+        ObservableList<String> semesterOnePlan = FXCollections.observableArrayList();
+        ObservableList<String> semesterTwoPlan = FXCollections.observableArrayList();
+        ObservableList<String> semesterThreePlan = FXCollections.observableArrayList();
+        ObservableList<String> semesterFourPlan = FXCollections.observableArrayList();
+        ObservableList<String> semesterFivePlan = FXCollections.observableArrayList();
+        ObservableList<String> semesterSixPlan = FXCollections.observableArrayList();
+        ObservableList<String> semesterSevenPlan = FXCollections.observableArrayList();
+        ObservableList<String> semesterEightPlan = FXCollections.observableArrayList();
+
+        for(Course course: s.displaySemesterbyInt(1)){
+            if(course != null)
+                semesterOnePlan.add(course.getTitle());
+            else
+                semesterOnePlan.add("No Courses");
         }
-        semesterOnePlanTable.setItems(courses);
+
+        for(Course course: s.displaySemesterbyInt(2)){
+            if(course != null)
+                semesterTwoPlan.add(course.getTitle());
+            else
+                semesterTwoPlan.add("No Courses");
+        }
+
+        for(Course course: s.displaySemesterbyInt(3)){
+            if(course != null)
+                semesterThreePlan.add(course.getTitle());
+            else
+                semesterThreePlan.add("No Courses");
+        }
+
+        for(Course course: s.displaySemesterbyInt(4)){
+            if(course != null)
+                semesterFourPlan.add(course.getTitle());
+            else
+                semesterFourPlan.add("No Courses");
+        }
+
+        for(Course course: s.displaySemesterbyInt(5)){
+            if(course != null)
+                semesterFivePlan.add(course.getTitle());
+            else
+                semesterFivePlan.add("No Courses");
+        }
+
+        for(Course course: s.displaySemesterbyInt(6)){
+            if(course != null)
+                semesterSixPlan.add(course.getTitle());
+            else
+                semesterSixPlan.add("No Courses");
+        }
+
+        for(Course course: s.displaySemesterbyInt(7)){
+            if(course != null)
+                semesterSevenPlan.add(course.getTitle());
+            else
+                semesterSevenPlan.add("No Courses");
+        }
+
+        for(Course course: s.displaySemesterbyInt(8)){
+            if(course != null)
+                semesterEightPlan.add(course.getTitle());
+            else
+                semesterEightPlan.add("No Courses");
+        }
+
+
+
+
+
+        if(semesterOneListView == null)
+            System.out.println("Semester One List View is null");
+        else
+            semesterOneListView.setItems(semesterOnePlan);
+
+        if(semesterTwoListView == null)
+            System.out.println("Semester Two List View is null");
+        else
+            semesterTwoListView.setItems(semesterTwoPlan);
+
+        if(semesterThreeListView == null)
+            System.out.println("Semester Three List View is null");
+        else
+            semesterThreeListView.setItems(semesterThreePlan);
+
+        if(semesterFourListView == null)
+            System.out.println("Semester Four List View is null");
+        else
+            semesterFourListView.setItems(semesterFourPlan);
+
+        if(semesterFiveListView == null)
+            System.out.println("Semester Five List View is null");
+        else
+            semesterFiveListView.setItems(semesterFivePlan);
+
+        if(semesterSixListView == null)
+            System.out.println("Semester Six List View is null");
+        else
+            semesterSixListView.setItems(semesterSixPlan);
+
+        if(semesterSevenListView == null)
+            System.out.println("Semester Seven List View is null");
+        else
+            semesterSevenListView.setItems(semesterSevenPlan);
+
+        if(semesterEightListView == null)
+            System.out.println("Semester Eight List View is null");
+        else
+            semesterEightListView.setItems(semesterEightPlan);
+
+
     }
+
 }
