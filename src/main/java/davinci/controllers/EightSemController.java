0a4c73814e1232acd6fee705eee09649ec0f619d
Jackson Dawson <jwd2464@gmail.com>
2024-04-16 14:00:11 -0400

    button for eightsemplan works

---
+++ src/main/java/davinci/controllers/EightSemController.java
@@ -0,0 +1,56 @@
+package davinci.controllers;
+import davinci.library.App;
+import davinci.model.User;
+import davinci.model.Student;
+import davinci.model.Major;
+import davinci.model.Course;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Observable;
+import java.util.ResourceBundle;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.control.TableView;
+
+
+public class EightSemController implements Initializable{
+    User u = App.user;
+    Student s = App.student;
+
+    @FXML
+    private TableView semesterOnePlanTable;
+
+    @FXML 
+    private TableView semesterTwoPlanTable;
+
+    @FXML
+    private TableView semesterThreePlanTable;
+
+    @FXML
+    private TableView semesterFourPlanTable;
+
+    @FXML
+    private TableView semesterFivePlanTable;
+
+    @FXML
+    private TableView semesterSixPlanTable;
+
+    @FXML
+    private TableView semesterSevenPlanTable;
+
+    @FXML
+    private TableView semesterEightPlanTable;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)  {
+        //semesterOnePlanTable.setItems(FXCollections.observableArrayList(s.displayEightSemesterPlan().get(0)));
+    }
+}
