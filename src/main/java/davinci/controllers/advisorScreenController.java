61fccd8688a592b557464bc78e90153307b6ec76
sphilips04 <sphilips@email.sc.edu>
2024-04-17 15:08:58 -0400

    added advisor to advisor controller

--- src/main/java/davinci/controllers/advisorScreenController.java
+++ src/main/java/davinci/controllers/advisorScreenController.java
@@ -2,9 +2,12 @@ package davinci.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import davinci.library.App;
+import davinci.model.Faculty;
+import davinci.model.Student;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -15,9 +18,11 @@ public class advisorScreenController implements Initializable {
     @FXML
     private Button logoutButton;
 
+    private Faculty advisor = App.faculty;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        String text = "test test test";
+        
     }
 
     @FXML
